389834101b432ef3359b7317f87b6e1b058af8e9
terminal-guy <vishaladhithya19@gmail.com>
2021-04-16 12:24:19 +0530

    spliting widgets into separate files

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
-  Answer(this.selectHandler);
+  Answer(this.selectHandler, this.answerText);
   final Function selectHandler;
+  final String answerText;
   @override
   Widget build(BuildContext context) {
     return Container(
         width: double.infinity,
         child: ElevatedButton(
-          child: Text('Answer 1'),
+          child: Text(answerText),
           onPressed: selectHandler,
           style: ButtonStyle(
             backgroundColor: MaterialStateProperty.all(Colors.blue),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/question.dart';
+import 'package:quiz_app/result.dart';
 import 'answer.dart';
+import 'quiz.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,46 +14,49 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  static const _questions = [
+    {
+      'questionText': 'what\'s your favourite color?',
+      'answers': ['black', 'red', 'white', 'blue']
+    },
+    {
+      'questionText': 'what\'s your favourite animal?',
+      'answers': ['panther', 'lion', 'tiger', 'bee']
+    },
+    {
+      'questionText': 'what\'s your favourite instructor?',
+      'answers': ['panther', 'lbry', 'tty', 'map']
+    },
+  ];
+
   var _questionIndex = 0;
+
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
+    if (_questionIndex < _questions.length) {
+      print('we have more questions');
+    } else {
+      print('no more questions');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'what\'s your favourite color?',
-        'answers': ['black', 'red', 'white', 'blue']
-      },
-      {
-        'questionText': 'what\'s your favourite animal?',
-        'answers': ['panther', 'lion', 'tiger', 'bee']
-      },
-      {
-        'questionText': 'what\'s your favourite instructor?',
-        'answers': ['panther', 'lbry', 'tty', 'map']
-      },
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
           title: Text('Quiz App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex],
-            ),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(),
       ),
     );
   }
